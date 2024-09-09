06ee5ab27a5ddd21fbeb4574ac322b309b2e877a
LordofTriton <agboola918@gmail.com>
2024-09-09 07:45:48 +0100

    Fixed more build issues.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 
 import React from 'react'
 
-import "./components/alert/alert.css"
-import "./components/chatWindow/chatWindow.css"
-import "./components/trainingWindow/trainingWindow.css"
-import "./components/infoPage/infoPage.css"
-import "./components/infoPage/infoPage.css"
-import "./components/offline/offline.css"
-import "./components/popMenu/popMenu.css"
-import "./components/title/title.css"
-import "./components/title/loader.css"
-import "./components/topNav/topNav.css"
+import "../components/alert/alert.css"
+import "../components/chatWindow/chatWindow.css"
+import "../components/trainingWindow/trainingWindow.css"
+import "../components/infoPage/infoPage.css"
+import "../components/infoPage/infoPage.css"
+import "../components/offline/offline.css"
+import "../components/popMenu/popMenu.css"
+import "../components/title/title.css"
+import "../components/title/loader.css"
+import "../components/topNav/topNav.css"
 import "./index.css"
 
 export default function App({ Component, pageProps }) {
